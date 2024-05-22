ae2fa042419473033ff6f6b664b14405d3e439ea
Rafi98 <33840574+Rafi98@users.noreply.github.com>
2024-05-23 03:19:09 +0530

    Update tmcr.sh

--- tmcr.sh
+++ tmcr.sh
@@ -18,8 +18,15 @@ TEAM1_DESCRIPTION="Team 1 description"
 TEAM2_DESCRIPTION="Team 2 description"
 
 # Create Team 1
-response1=$(curl -X POST \
-  -H "Authorization: token $TOKEN" \
+#response1=$(curl -X POST \
+ # -H "Authorization: token $TOKEN" \
+  #-d "{\"name\": \"$TEAM1_NAME\", \"description\": \"$TEAM1_DESCRIPTION\"}" \
+  #"https://api.github.com/orgs/$ORGANIZATION/teams")
+
+  response1=$(curl -X POST -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: token $TOKEN"" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
   -d "{\"name\": \"$TEAM1_NAME\", \"description\": \"$TEAM1_DESCRIPTION\"}" \
   "https://api.github.com/orgs/$ORGANIZATION/teams")
 
