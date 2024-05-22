2ab1938029cfb31b19291d22fbd56bf0795e4738
Rafi98 <33840574+Rafi98@users.noreply.github.com>
2024-05-23 02:35:17 +0530

    Update tmcr.sh

--- tmcr.sh
+++ tmcr.sh
@@ -8,4 +8,24 @@ ORGANIZATION="RafiCisco"
 #TOKEN="${{ secrets.GH_TOKEN }}"
 #TOKEN="${{ secrets.GH_ORGT }}"
 
+# Create Admin Team
+ADMIN_TEAM_ID=$(curl -X POST -H "Authorization: token $TOKEN" -d '{"name": "admin", "permission": "admin"}' "https://api.github.com/orgs/$ORGANIZATION/teams" | jq -r '.id')
+
+# Create Dev Team
+DEV_TEAM_ID=$(curl -X POST -H "Authorization: token $TOKEN" -d '{"name": "dev", "permission": "push"}' "https://api.github.com/orgs/$ORGANIZATION/teams" | jq -r '.id')
+
+# Get list of repositories
+REPO_LIST=$(curl -H "Authorization: token $TOKEN" "https://api.github.com/orgs/$ORGANIZATION/repos" | jq -r '.[].name')
+
+# Assign Admin Team to Repositories
+for REPO in $REPO_LIST; do
+    curl -X PUT -H "Authorization: token $TOKEN" -d "{\"permission\": \"admin\"}" "https://api.github.com/teams/$ADMIN_TEAM_ID/repos/$ORGANIZATION/$REPO"
+done
+
+# Assign Dev Team to Repositories
+for REPO in $REPO_LIST; do
+    curl -X PUT -H "Authorization: token $TOKEN" -d "{\"permission\": \"push\"}" "https://api.github.com/teams/$DEV_TEAM_ID/repos/$ORGANIZATION/$REPO"
+done
+
+echo "Teams assigned to repositories successfully!"
 
