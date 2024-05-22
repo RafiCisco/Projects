f36f5b3557e309d3a12856cd7de5ab0621f0d867
Rafi98 <33840574+Rafi98@users.noreply.github.com>
2024-05-23 02:50:00 +0530

    Update tmcr.sh

--- tmcr.sh
+++ tmcr.sh
@@ -3,6 +3,14 @@
 # GitHub Organization or username
 ORGANIZATION="RafiCisco"
 
+# Access GitHub token passed from workflow
+TOKEN="${GITHUB_TOKEN}"
+
+# Now you can use $TOKEN in your script for making GitHub API requests or other operations
+echo "GitHub Token: $TOKEN"
+
+
+
 # GitHub Token with appropriate permissions
 #TOKEN="${{ secrets.GH_PAT }}"  # Or your personal access token
 #TOKEN="${{ secrets.GH_TOKEN }}"
