0d85819956aa745a3f46decfafd6a8a453adf5c3
Rafi98 <33840574+Rafi98@users.noreply.github.com>
2024-05-22 16:13:04 +0530

    Update tmcr.sh

--- tmcr.sh
+++ tmcr.sh
@@ -2,7 +2,7 @@
 
 # Variables
 GITHUB_ORG="RafiCisco"
-GITHUB_TOKEN="GH_PAT"
+GITHUB_TOKEN="GH_TOKEN"
 
 # Define repositories and their corresponding teams and permissions
 declare -A repos_teams=(
