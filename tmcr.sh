b00b956af8d33bb56ed438fbc8e9d54eafb22297
Rafi98 <33840574+Rafi98@users.noreply.github.com>
2024-05-23 03:08:30 +0530

    Update tmcr.sh

--- tmcr.sh
+++ tmcr.sh
@@ -9,6 +9,10 @@ TOKEN="$1"
 # Now you can use $TOKEN in your script for making GitHub API requests or other operations
 echo "GitHub Token: $TOKEN"
 
+# Create Admin Team
+ADMIN_TEAM_ID=$(curl -X POST -H "Authorization: token $TOKEN" -d '{"name": "admin", "permission": "admin"}' "https://api.github.com/orgs/$ORGANIZATION/teams" | jq -r '.id')
+
+
 
 
 
