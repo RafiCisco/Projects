c0d0a81170087b4c2d1729da9d34cf30fdca3174
Rafi98 <33840574+Rafi98@users.noreply.github.com>
2024-05-23 23:21:56 +0530

    Update tmcr.sh

--- tmcr.sh
+++ tmcr.sh
@@ -11,9 +11,8 @@ ORGANIZATION="RafiCisco"
 TOKEN="${GITHUB_TOKEN}"
 
 
-# Variables
-TEAM_NAME="admin"  # change the name and description if team name is already exists
-TEAM_DESCRIPTION="Admin with full access"
+TEAM_NAME="Admin" # Change team name if already exists
+TEAM_DESCRIPTION="Admin team with full access"
 TEAM_PRIVACY="closed"  # or "secret"
 
 # Function to create a team
@@ -27,7 +26,7 @@ create_team() {
     -H "Content-Type: application/json" \
     -d "{\"name\": \"$team_name\", \"description\": \"$team_description\", \"privacy\": \"$team_privacy\"}" \
     "https://api.github.com/orgs/$ORGANIZATION/teams")
-  
+
   local team_id=$(echo "$response" | jq -r '.id')
   local error_message=$(echo "$response" | jq -r '.message')
 
@@ -35,23 +34,35 @@ create_team() {
     echo "Error creating team $team_name: $error_message"
     exit 1
   else
-    echo "Team '$team_name' created with ID $team_id"
     echo "$team_id"
   fi
 }
 
-# Create the team
-#create_team "$TEAM_NAME" "$TEAM_DESCRIPTION" "$TEAM_PRIVACY"
+# Function to get team slug from team ID
+get_team_slug() {
+  local team_id=$1
 
+  local response=$(curl -s -H "Authorization: token $GITHUB_TOKEN" \
+    "https://api.github.com/orgs/$ORGANIZATION/teams")
+
+  local team_slug=$(echo "$response" | jq -r ".[] | select(.id==$team_id) | .slug")
+
+  if [[ -z "$team_slug" ]]; then
+    echo "Error: Team slug not found for team ID $team_id"
+    exit 1
+  else
+    echo "$team_slug"
+  fi
+}
 
 # Function to get team details
 get_team_details() {
-  local team_id=$1
+  local team_slug=$1
 
   local response=$(curl -s -X GET \
     -H "Authorization: token $GITHUB_TOKEN" \
     -H "Content-Type: application/json" \
-    "https://api.github.com/organizations/$ORGANIZATION/team/$team_id")
+    "https://api.github.com/orgs/$ORGANIZATION/teams/$team_slug")
 
   echo "$response" | jq '.'
 }
@@ -59,5 +70,9 @@ get_team_details() {
 # Create the team and get its details
 TEAM_ID=$(create_team "$TEAM_NAME" "$TEAM_DESCRIPTION" "$TEAM_PRIVACY")
 echo "Team '$TEAM_NAME' created with ID $TEAM_ID"
-echo "Team details:"
-get_team_details "$TEAM_ID"
+
+# Fetch the team slug using the team ID
+TEAM_SLUG=$(get_team_slug "$TEAM_ID")
+
+echo "Fetching details for team '$TEAM_NAME' with slug '$TEAM_SLUG'..."
+get_team_details "$TEAM_SLUG"
